a5e555d48a9109678a1f376590fdbca24e95fa7a
waqasansarii <65023462+waqasansarii@users.noreply.github.com>
2021-09-17 01:06:04 +0500

    update terms page

--- src/Pages/Terms/Terms.jsx
+++ src/Pages/Terms/Terms.jsx
@@ -1,30 +1,14 @@
-import React, { useLayoutEffect,  useState } from "react";
-import arrow from '../../Assets/Icon feather-arrow-right@2x.png'
+import React, { useLayoutEffect, useState } from "react";
+import arrow from "../../Assets/Icon feather-arrow-right@2x.png";
 import { Link } from "react-router-dom";
 import "./Terms.css";
 import SubNav from "../../Layout/SubNav";
+import rightImg from "../../Assets/undraw_contract_uy56.png";
+import TermsDetail from "./TermsDetail";
 
-const termsData = [
-  {
-    head: "LLorem ipsum dolo",
-    para: " Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea",
-    id: "01",
-  },
-  {
-    head: "OWNERSHIT,COPYRIGHT AND TRADEMARK NOTICES",
-    para: " Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea",
-    id: "02",
-    link:''
-  },
-  {
-    head: "WARRANTY DISCLAIMER",
-    para: " Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea",
-    id: "03",
-  },
-];
+
 
 const TermAndCond = () => {
-  let [trans, setTrans] = useState([]);
 
   useLayoutEffect(() => {
     window.scrollTo(0, 0);
@@ -32,45 +16,37 @@ const TermAndCond = () => {
 
   
 
-  const handleTransalte = (event) => {
-    let filter = termsData.filter((val) => val.id === event);
-    setTrans(filter);
-    // console.log(trans);
-  };
-
   return (
     <div className="terms_and_cond_container">
-      <SubNav head='Terms of use' />
-      <div className="terms_and_cond_main_div">
-        <div className="terms_boxex_div">
-          <div
-            className="terms_one_box_div" >
-            {termsData.map((val) => (
-              <div className={`term_main "shade_box_l" `} key={val.id} onClick={() => handleTransalte(val.id)}>
-                <p className="term_note_para">{val.head}</p>
-                <div className="terms_para_div">
-                  <p> {val.para} </p>
-                </div>
-                <div className="learm_more">
-                   <Link className='more_link'>Learn more <img src={arrow} alt="" /></Link>
-                </div>
-              </div>
-            ))}
-          </div>
+      <SubNav head="Terms of use" />
+      <div className="mobileView_terms_top_img">
+        <img src={rightImg} alt="" />
+      </div>
+      <div className="terms_top_main">
+        <p className="mt-3">
+          This Agreement is between the user of iPurvey service and The Digital
+          Purveyor Company Limited (trading as iPurvey). The Digital Purveyor
+          Company Limited is a company registered in England under company
+          number 12484804, at address 130 Old Street, London, England, EC1V 9BD.
+        </p>
+        <p className="mt-3">
+          Please read these Terms and Conditions and our Privacy Policy
+          carefully as by using the site or our service, you confirm that you
+          accept these terms and agree to abide by them.
+        </p>
+        <p className="mt-3">
+          These terms may change at any time and changes will be effective
+          immediately upon posting. If you continue using the site or our
+          service you agree to the new terms.
+        </p>
+      </div>
+      <div className="terms_cond_main_div">
+        <div className="terms_ques_content_main">
+          <TermsDetail />
         </div>
         <div className="terms_two_div select_lang_div">
-          <div
-            className="terms_two_box_para_div"   >
-            <div className="terms_sub_head_div">
-              <h5>
-                <strong>In Plain English</strong>
-              </h5>
-            </div>
-            { trans.map((val,i) => (
-              <div key={i}>
-                <p className='trans_para'> {val.para} </p>
-              </div>
-            ))}
+          <div className="terms_two_img_box">
+            <img src={rightImg} alt="..." />
           </div>
         </div>
       </div>
---
+++ src/Pages/Terms/TermsDetail.jsx
@@ -0,0 +1,205 @@
+import React, { useState } from "react";
+import arrow from "../../Assets/Path 5677@3x.png";
+
+const termsData = [
+  {
+    head: "Summary",
+    para: [
+      `By using the services provided by iPurvey, you are agreeing to
+      our Terms & Conditions as set out`,
+      `Your access to and use of the service is subject to our Privacy
+      Policy iPurvey is not responsible for any errors or omissions
+      within the data provided to us by third- party. We utilise the
+      information as provided to us to monitor your journey for
+      disruption. You are responsible for your actions and we are not
+      liable for any damages or actions taken based on the use of the
+      data or the application or service. iPurvey is not responsible
+      for the content of third-party websites and you take full
+      responsibility for accepting the terms and conditions of the
+      third-party web sites.`,
+    ],
+    id: "01",
+  },
+  {
+    head: "Who we are",
+    para: [
+      `By using the services provided by iPurvey, you are agreeing to
+      our Terms & Conditions as set out`,
+      `Your access to and use of the service is subject to our Privacy
+      Policy iPurvey is not responsible for any errors or omissions
+      within the data provided to us by third- party. We utilise the
+      information as provided to us to monitor your journey for
+      disruption. You are responsible for your actions and we are not
+      liable for any damages or actions taken based on the use of the
+      data or the application or service. iPurvey is not responsible
+      for the content of third-party websites and you take full
+      responsibility for accepting the terms and conditions of the
+      third-party web sites.`,
+    ],
+    id: "02",
+    link: "",
+  },
+  {
+    head: "How do we do this",
+    para: [
+      `By using the services provided by iPurvey, you are agreeing to
+      our Terms & Conditions as set out`,
+      `Your access to and use of the service is subject to our Privacy
+      Policy iPurvey is not responsible for any errors or omissions
+      within the data provided to us by third- party. We utilise the
+      information as provided to us to monitor your journey for
+      disruption. You are responsible for your actions and we are not
+      liable for any damages or actions taken based on the use of the
+      data or the application or service. iPurvey is not responsible
+      for the content of third-party websites and you take full
+      responsibility for accepting the terms and conditions of the
+      third-party web sites.`,
+    ],
+    id: "03",
+  },
+  {
+    head: "What is covered?",
+    para: [
+      `By using the services provided by iPurvey, you are agreeing to
+      our Terms & Conditions as set out`,
+      `Your access to and use of the service is subject to our Privacy
+      Policy iPurvey is not responsible for any errors or omissions
+      within the data provided to us by third- party. We utilise the
+      information as provided to us to monitor your journey for
+      disruption. You are responsible for your actions and we are not
+      liable for any damages or actions taken based on the use of the
+      data or the application or service. iPurvey is not responsible
+      for the content of third-party websites and you take full
+      responsibility for accepting the terms and conditions of the
+      third-party web sites.`,
+    ],
+    id: "04",
+  },
+  {
+    head: "Limitation of our Liability",
+    para: [
+      `By using the services provided by iPurvey, you are agreeing to
+      our Terms & Conditions as set out`,
+      `Your access to and use of the service is subject to our Privacy
+      Policy iPurvey is not responsible for any errors or omissions
+      within the data provided to us by third- party. We utilise the
+      information as provided to us to monitor your journey for
+      disruption. You are responsible for your actions and we are not
+      liable for any damages or actions taken based on the use of the
+      data or the application or service. iPurvey is not responsible
+      for the content of third-party websites and you take full
+      responsibility for accepting the terms and conditions of the
+      third-party web sites.`,
+    ],
+    id: "05",
+  },
+  {
+    head: "Links and Third-Party Websites",
+    para: [
+      `By using the services provided by iPurvey, you are agreeing to
+      our Terms & Conditions as set out`,
+      `Your access to and use of the service is subject to our Privacy
+      Policy iPurvey is not responsible for any errors or omissions
+      within the data provided to us by third- party. We utilise the
+      information as provided to us to monitor your journey for
+      disruption. You are responsible for your actions and we are not
+      liable for any damages or actions taken based on the use of the
+      data or the application or service. iPurvey is not responsible
+      for the content of third-party websites and you take full
+      responsibility for accepting the terms and conditions of the
+      third-party web sites.`,
+    ],
+    id: "06",
+  },
+  {
+    head: "Compensation",
+    para: [
+      `By using the services provided by iPurvey, you are agreeing to
+      our Terms & Conditions as set out`,
+      `Your access to and use of the service is subject to our Privacy
+      Policy iPurvey is not responsible for any errors or omissions
+      within the data provided to us by third- party. We utilise the
+      information as provided to us to monitor your journey for
+      disruption. You are responsible for your actions and we are not
+      liable for any damages or actions taken based on the use of the
+      data or the application or service. iPurvey is not responsible
+      for the content of third-party websites and you take full
+      responsibility for accepting the terms and conditions of the
+      third-party web sites.`,
+    ],
+    id: "07",
+  },
+];
+
+const TermsDetail = () => {
+  let [trans, setTrans] = useState([termsData[0]]);
+
+  const handleTransalte = (event) => {
+    let filter = termsData.filter((val) => val.id === event);
+    setTrans(filter);
+    if (trans.length && filter[0].id === trans[0].id) {
+      setTrans([]);
+    }
+  };
+
+  return (
+    <div className="terms_detail_contianer">
+      <div className="terms_dtl_md">
+        <div className="termsDetail_box">
+          <div className="termsQues_list">
+            <h4>Content</h4>
+            <ul className="tersmQues_ul">
+              {termsData.map((val) => (
+                <li>
+                  <div
+                    className={`termQ
+                       ${
+                         trans.length
+                           ? trans[0].id === val.id
+                             ? "active_term"
+                             : ""
+                           : null
+                       }
+                       `}
+                    onClick={() => handleTransalte(val.id)}
+                  >
+                    <p>{val.head}</p>
+                    {trans.length? val.id === trans[0].id ? (
+                      <img src={arrow} alt="..." />
+                    ) : null : null}
+                  </div>
+                  {trans.length?  trans[0].id === val.id
+                    ? trans.map((val) => (
+                        <div className="mobileView_termsQues_detail">
+                          <h6>{val.head}</h6>
+                          {val.para.map((para) => (
+                            <>
+                              <p className="mt-3">{para}</p>
+                            </>
+                          ))}
+                        </div>
+                      ))
+                    : null : null }
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="terms_quesDetail_box">
+            {trans.map((val) => (
+              <div className="termQues_detail_content">
+                <h6>{val.head}</h6>
+                {val.para.map((para) => (
+                  <>
+                    <p className="mt-3">{para}</p>
+                  </>
+                ))}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TermsDetail;
